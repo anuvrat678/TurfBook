aafa910af113954b461b9799cea6f2f0864dd067
anuvrat678 <anuvratrajpurohit@gmail.com>
2025-10-30 22:23:53 +0530

    Add Vercel rewrite for SPA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,21 +2,21 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables
   const env = loadEnv(mode, process.cwd(), '');
   
-  const apiUrl = env.VITE_API_URL;
-
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: apiUrl,
+          target: env.VITE_API_URL,
           changeOrigin: true,
           secure: false,
         },
       },
     },
+    build: {
+      outDir: 'dist'
+    }
   };
 });
